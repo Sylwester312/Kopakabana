787d330f17ab734866c74c5af65be244fb2d5ddf
Sylwester312 <sylwester.kowalewski12@gmail.com>
2023-05-25 14:23:48 +0200

    Dodaj pliki projektów.

---
+++ Kopakabana/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kopakabana
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSedzia_Click(object sender, RoutedEventArgs e)
+        {
+            OpcjeSedziow opcjeSedziow = new OpcjeSedziow();
+            opcjeSedziow.ShowDialog();
+        }
+
+        private void ButtonDruzyna_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonRozrywka_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Kopakabana/OpcjeSedziow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kopakabana
+{
+    /// <summary>
+    /// Logika interakcji dla klasy OpcjeSedziow.xaml
+    /// </summary>
+    public partial class OpcjeSedziow : Window
+    {
+        public OpcjeSedziow()
+        {
+            InitializeComponent();
+        }
+
+        private void DodajSedziego_Click(object sender, RoutedEventArgs e)
+        {
+            DodajSedziego oknosedzia = new DodajSedziego();
+            oknosedzia.ShowDialog();
+            
+        }
+    }
+}
